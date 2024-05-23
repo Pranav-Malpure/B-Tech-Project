a6a00a8af381960cffe5fadac8ec4d09e604d60f
Pranav-Malpure <76242161+Pranav-Malpure@users.noreply.github.com>
2024-05-23 16:14:36 +0530

    deleted extra files

--- inverse_kinematics/arm_composer.py
+++
@@ -1,8 +0,0 @@
-from dm_control import composer
-from dm_control.composer.observation import observable
-
-class manipulator_RL_task():
-    "A 6dof robotic arm of kinova manipulator."
-    def _build(self):
-        # self.__model = 
-        pass
